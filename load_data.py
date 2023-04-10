17b5af3118550c9bcc135d30bcbb448cdfd68bed
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2023-04-10 18:57:39 +0800

    update

--- load_data.py
+++ load_data.py
@@ -5,6 +5,9 @@ import torch
 from torch_geometric.utils import from_networkx
 import torch.nn.functional as F
 from utils import normalize_adj, sparse_diag, covariance_transform, correlation_readout
+from category_encoders import CatBoostEncoder
+import numpy as np
+from sklearn import preprocessing
 
 
 class Data():
@@ -16,16 +19,11 @@ class Data():
             path (str): file path
             dataset (str): dataset name
         """
-        feature = pd.read_csv(path + dataset + '/X.csv').values
-        self.feature = torch.tensor(feature, dtype=torch.float).nan_to_num()
-        # normalization
-        # self.feature = F.normalize(self.feature, p=1, dim=1)
-        self.feature = (self.feature - self.feature.mean(dim=1, keepdim=True)) / self.feature.std(dim=1, keepdim=True)
-        self.n_feature = self.feature.shape[1]
-        self.feature_cov = covariance_transform(self.feature)  # n * d * d
-        self.feature_corr = correlation_readout(self.feature_cov.sum(0, keepdim=True))
+        feature = pd.read_csv(path + dataset + '/X.csv').fillna(0).values
+
+
         label = pd.read_csv(path + dataset + '/y.csv').values
-        
+     
         if dataset in ['house_class', 'vk_class']:
             self.task = 'classification'
             self.label = torch.tensor(label, dtype=torch.long).squeeze(1)
@@ -40,6 +38,31 @@ class Data():
         self.idx_train = masks['0']['train']
         self.idx_val = masks['0']['test']
         self.idx_test = masks['0']['test']
+
+
+        if dataset == 'avazu':
+            # with open(path + dataset +  '/cat_features.txt') as f:
+            #     cat_features = f.read().splitlines() 
+            # cat_features = np.where(columns.isin(categorical_columns))[0]
+            feature = feature.astype(str)
+            cat_features = np.arange(feature.shape[1])
+            enc = CatBoostEncoder()
+            feature[np.ix_(self.idx_train, cat_features)] = enc.fit_transform(feature[np.ix_(self.idx_train, cat_features)], label[self.idx_train])
+            feature[np.ix_(self.idx_val + self.idx_test, cat_features)] = enc.transform(feature[np.ix_(self.idx_val + self.idx_test, cat_features)])
+            feature = feature.astype(float)
+
+        # normalization
+        min_max_scaler = preprocessing.MinMaxScaler()
+        feature[self.idx_train] = min_max_scaler.fit_transform(feature[self.idx_train])
+        feature[self.idx_val + self.idx_test] = min_max_scaler.transform(feature[self.idx_val + self.idx_test])
+
+
+        self.feature = torch.tensor(feature, dtype=torch.float)
+        self.n_feature = self.feature.shape[1]
+        self.feature_cov = covariance_transform(self.feature)  # n * d * d
+        self.feature_corr = correlation_readout(self.feature_cov.sum(0, keepdim=True))
+
+
 
         G = nx.read_graphml(path + dataset + '/graph.graphml', node_type=int)
         G.graph = {}    # prevent the error of loading graph using from_networkx from pyg
